25d24d368d10abb760eaa25e09af96fb0b21a5c1
shouzhuoyi <3175882821@qq.com>
2026-02-10 18:26:57 +0800

    wtf have i done

--- AlgoAnalysis_hhy/1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
-  for (int i = 1; i <= 11; i++) {
-    printf("#include(\"chaptersTypst/chapter%d.typ\")\n", i);
-  }
-}
--- AlgoAnalysis_hhy/test_on_my_package/publish.sh
+++
@@ -1,8 +0,0 @@
-PKG_NAME="ysz_tools"
-VERSION="0.1.0"
-# macOS specific path for Typst local packages
-DEST="$HOME/Library/Application Support/typst/packages/local/$PKG_NAME/$VERSION"
-
-mkdir -p "$DEST"
-cp lib.typ typst.toml "$DEST"
-echo "已打包至 @local/$PKG_NAME:$VERSION"
